1a9f6db834fcd02bef95cfa3b6ee2260ffea4009
Victory Bedru <victorybedru@gmail.com>
2026-02-09 02:57:34 -0800

    Initial commit: repo skeleton

---
+++ front-end/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import '../app.css';
+  import Navbar from '$lib/components/layout/Navbar.svelte';
+  import Footer from '$lib/components/layout/Footer.svelte';
+</script>
+
+<Navbar />
+<main>
+  <slot />  <!-- Your page content (home, login, etc.) will appear here -->
+</main>
+<Footer />
